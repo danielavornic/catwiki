dce6dd02c68e99dc91d6c7d3c81f9cda942b0cc6
Daniela Vornic <daniela.vornic@gmail.com>
2022-08-25 09:26:53 +0300

    Add material ui icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Poppins:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
       </Head>
       <body>
         <Main />
