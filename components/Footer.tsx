8105eee0f7da716eca1fc5786f1ec45d8881ca95
Daniela Vornic <daniela.vornic@gmail.com>
2022-08-26 10:29:22 +0300

    Increase margin-top of footer for 2xl screens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Footer = () => {
   return (
-    <footer className="container mx-auto px-4 w-full md:px-2 mt-10 lg:mt-14 xl:mt-[100px]">
+    <footer className="container mx-auto px-4 w-full md:px-2 mt-10 lg:mt-14 xl:mt-[100px] 2xl:mt[120px]">
       <div className="bg-black text-white rounded-t-[36px] pt-8 pb-6 px-6 md:px-12">
         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
           <Link href="/">
