6dbf08f0032cd01d1f28ead27168dff935adf946
Daniela Vornic <daniela.vornic@gmail.com>
2022-08-26 15:26:45 +0300

    Increase margins of header and footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header className="container mx-auto p-4 mb-4 md:px-2">
+    <header className="container mx-auto p-4 mb-4 lg:mb-10 md:px-2">
       <Link href="/">
         <a>
           <img src="/images/catwiki-logo.svg" />
